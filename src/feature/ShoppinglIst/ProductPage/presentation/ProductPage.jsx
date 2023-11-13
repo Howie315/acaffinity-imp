02925daf55256c211ace39489dbf91a6399a00c3
Howie315 <nguyen.howie2010@gmail.com>
2023-11-12 22:15:27 -0800

    added the slideshow

--- src/feature/ShoppinglIst/ProductPage/presentation/ProductPage.jsx
+++ src/feature/ShoppinglIst/ProductPage/presentation/ProductPage.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { productsData } from "../../Products/domain/ProductDomain";
 import './ProductPage.scss'; 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from 'react-slick'; // Import the Slider component
+
+
 
 const ProductPage = () => {
   const { productId } = useParams();
@@ -11,6 +16,22 @@ const ProductPage = () => {
     return <div>Product not found</div>;
   }
 
+  const imagesToDisplay = [...product.additionalImages];
+
+   // Slider settings
+   const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1, // Show three items at once
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    cssEase: "linear",
+    centerMode: true, // Enable center mode
+    centerPadding: '60px', // Add padding on the sides
+  };
+  
   return (
     <div className="productPage">
       <h1 className="productPage__title">{product.name}</h1>
@@ -18,7 +39,22 @@ const ProductPage = () => {
       <div className="productPage__imageContainer">
         <img src={product.imageUrl} alt={product.name} className="productPage__image" />
       </div>
-      {/* Add more details here as necessary */}
+      <button className = "buy">Buy</button>
+      <p className = "price"> {product.price}</p>
+
+
+<p className = "Highlights">Get the highlights.</p>
+
+      <Slider {...settings}>
+        {imagesToDisplay.map((image, index) => (
+      <div key={index} className="productPage__slide">
+          <div  className="productPage__imageContainer2">
+            <img src={image} alt={`${product.name} ${index + 1}`} className="productPage__image2"
+             />
+          </div>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
--- src/feature/ShoppinglIst/Products/domain/ProductDomain.js
+++ src/feature/ShoppinglIst/Products/domain/ProductDomain.js
@@ -1,16 +1,29 @@
-// ProductDomain.js
-
 import waterSprayer from "../../../../img/waterSprayer.jpg";
 import carbonfilterKit from "../../../../img/carbonfilterKit.jpg";
 import meterKit from "../../../../img/meterKit.jpg";
+import sprayer1 from "../../../../img/sprayer1.jpg";
+import sprayer2 from "../../../../img/sprayer2.jpg";
+import sprayer3 from "../../../../img/sprayer3.jpg";
+import sprayer4 from "../../../../img/sprayer4.jpg";
+import sprayer5 from "../../../../img/sprayer5.jpg";
 
 export class Product {
-  constructor(id, name, description, imageUrl, learnMore) {
+  constructor(
+    id,
+    name,
+    description,
+    imageUrl,
+    learnMore,
+    price,
+    additionalImages
+  ) {
     this.id = id;
     this.name = name;
     this.description = description;
     this.imageUrl = imageUrl;
     this.learnMore = learnMore;
+    this.price = price;
+    this.additionalImages = additionalImages;
   }
 }
 
@@ -21,21 +34,25 @@ export const productsData = [
     "AUTOMATIC WATER SPRAYER",
     "Mind-blowing. Head-turning",
     waterSprayer,
-    "/products/1" // Path to the detail page of this product
+    "/products/1",
+    "29.99",
+    [sprayer1, sprayer2, sprayer3, sprayer4, sprayer5]
   ),
   new Product(
     2,
     "HYDROPONIC METER PRO KIT",
     "Packed with more juice",
     meterKit,
-    "/products/2" // Path to the detail page of this product
+    "/products/2",
+    "149.99"
   ),
   new Product(
     3,
     "REFILLABLE CARBON FILTER KIT",
     "Stronger than ever",
     carbonfilterKit,
-    "/products/3" // Path to the detail page of this product
+    "/products/3",
+    "79.99"
   ),
   // ... other products
 ];
