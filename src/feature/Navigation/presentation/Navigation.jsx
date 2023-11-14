e305c27cb7274b4fedc2b9fd093de2e58623d94c
Howie Nguyen <nguyen.howie2010@gmail.com>
2023-11-14 11:33:04 -0800

    added cool animation for the navbar

--- src/feature/Navigation/presentation/Navigation.jsx
+++ src/feature/Navigation/presentation/Navigation.jsx
@@ -1,19 +1,36 @@
 // NavBar.js
-import React from 'react';
-import './Navbar.css'; // Importing the CSS file
+import React from "react";
+import "./Navbar.css"; // Importing the CSS file
 
 const NavBar = () => {
-    return (
-        <nav className="navbar">
-            <div className="navbar-brand">AC Infinity</div>
-            <ul className="nav-links">
-                <li><a href="/">Home</a></li>
-                <li><a href="/products">Products</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-    );
+	window.addEventListener("scroll", () => {
+		const navbar = document.querySelector(".navbar");
+		if (window.scrollY > 50) {
+			navbar.classList.add("scrolled");
+		} else {
+			navbar.classList.remove("scrolled");
+		}
+	});
+
+	return (
+		<nav className="navbar">
+			<div className="navbar-brand">AC Infinity</div>
+			<ul className="nav-links">
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="/products">Products</a>
+				</li>
+				<li>
+					<a href="/about">About</a>
+				</li>
+				<li>
+					<a href="/contact">Contact</a>
+				</li>
+			</ul>
+		</nav>
+	);
 };
 
 export default NavBar;
