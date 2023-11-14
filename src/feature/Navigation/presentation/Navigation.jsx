445d092fba265d7ab063b8710436aa73ab5266bf
Howie Nguyen <nguyen.howie2010@gmail.com>
2023-11-14 15:54:53 -0800

    added expandable navbar

--- src/feature/Navigation/presentation/Navigation.jsx
+++ src/feature/Navigation/presentation/Navigation.jsx
@@ -1,16 +1,23 @@
-// NavBar.js
-import React from "react";
-import "./Navbar.css"; // Importing the CSS file
+import React, { useEffect } from "react";
+import "./Navbar.css";
 
 const NavBar = () => {
-	window.addEventListener("scroll", () => {
-		const navbar = document.querySelector(".navbar");
-		if (window.scrollY > 50) {
-			navbar.classList.add("scrolled");
-		} else {
-			navbar.classList.remove("scrolled");
-		}
-	});
+	useEffect(() => {
+		const handleScroll = () => {
+			const navbar = document.querySelector(".navbar");
+			if (window.scrollY > 50) {
+				navbar.classList.add("scrolled");
+			} else {
+				navbar.classList.remove("scrolled");
+			}
+		};
+
+		window.addEventListener("scroll", handleScroll);
+
+		return () => {
+			window.removeEventListener("scroll", handleScroll);
+		};
+	}, []);
 
 	return (
 		<nav className="navbar">
@@ -19,8 +26,13 @@ const NavBar = () => {
 				<li>
 					<a href="/">Home</a>
 				</li>
-				<li>
-					<a href="/products">Products</a>
+				<li className="expandable">
+					<a href="/">Products</a>
+					<div className="hidden-links">
+						<a href="/purchase/waterSprayer">Water Sprayer</a>
+						<a href="/purchase/meterKit">Meter Kit</a>
+						<a href="/purchase/carbonFilter">Carbon Filter Kit</a>
+					</div>
 				</li>
 				<li>
 					<a href="/about">About</a>
